2870887f6ac58aca92442783254eb34d2fb5205d
Grzegorz (vara) Warywoda <war29@wp.pl>
2008-08-29 20:18:03 +0200

    choose mode (silent or verbose)

--- src/config/Configuration.java
+++ src/config/Configuration.java
@@ -13,7 +13,7 @@ import java.awt.Toolkit;
  * @author vara
  */
 public class Configuration {
-    
+    private static boolean modeVerbose = false;
     public static final String OS_WINDOWS = "WINDOWS";
     public static final String OS_LINUX = "LINUX";
     public static final String OS_OTHER = "OTHER";
@@ -35,5 +35,19 @@ public class Configuration {
 	    return Configuration.OS_LINUX;
 	}else
 	    return Configuration.OS_OTHER;
+    }
+    
+    public static void setMode(boolean verbose)
+    {
+	if(verbose)
+	   System.out.println("Mode Set loudly ! aaaaaa");
+	else
+	    System.out.println("set to silent mode");
+	
+	modeVerbose = verbose;
+    }
+    public static synchronized boolean getMode()
+    {
+	return modeVerbose;
     }
 }
