6c38009667f1e82d1c17a18c0afd777602767c95
Grzegorz (vara) Warywoda <war29@wp.pl>
2008-08-30 16:15:41 +0200

    added new functions

--- src/app/utils/Utils.java
+++ src/app/utils/Utils.java
@@ -7,8 +7,11 @@ package app.utils;
 
 import java.awt.Color;
 import javax.swing.BorderFactory;
-import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import org.apache.batik.dom.svg.SVGOMPoint;
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGLocatable;
+import org.w3c.dom.svg.SVGMatrix;
 
 /**
  *
@@ -16,15 +19,33 @@ import javax.swing.border.Border;
  */
 public class Utils {
 
-    public static Border createSimpleBorder(int top,int left,int bottom,int right,Color colFrame)
+    public static Border createOutsiderBorder(int top,int left,int bottom,int right,Color colFrame)
     {     
         return BorderFactory.createCompoundBorder(
-			    BorderFactory.createEtchedBorder(BevelBorder.LOWERED,colFrame,Color.LIGHT_GRAY),
+			    BorderFactory.createLineBorder(colFrame),
 			    BorderFactory.createEmptyBorder(
 				top,   // number of points from the top
 				left,   // number of points along the left side
 				bottom,    // number of points from the bottom
 				right)  // number of points on the right side
 				);
+    }
+    public static Border createInsiderBorder(int top,int left,int bottom,int right,Color colFrame)
+    {     
+        return BorderFactory.createCompoundBorder(
+			    BorderFactory.createEmptyBorder(
+				top,   // number of points from the top
+				left,   // number of points along the left side
+				bottom,    // number of points from the bottom
+				right),  // number of points on the right side				
+			    BorderFactory.createLineBorder(colFrame));
+    }
+    
+    public static SVGOMPoint getLocalPointFromDomElement(Element element, int x, int y) 
+    {	    
+	    SVGMatrix mat = ((SVGLocatable) element).getScreenCTM();
+	    SVGMatrix imat = mat.inverse(); // screen -> elem
+	    SVGOMPoint pt = new SVGOMPoint(x, y);
+	    return (SVGOMPoint) pt.matrixTransform(imat);
     }
 }
