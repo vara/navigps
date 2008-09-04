41fd4d3db177d617f207450db97b7f459ff5cf23
Grzegorz (vara) Warywoda <war29@wp.pl>
2008-09-04 08:17:19 +0200

    added style and tab attrybute to textpane for dispaly verbose

--- src/app/utils/JTextPaneForVerboseInfo.java
+++ src/app/utils/JTextPaneForVerboseInfo.java
@@ -5,9 +5,19 @@
 
 package app.utils;
 
-import java.awt.GridBagLayout;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.TabSet;
+import javax.swing.text.TabStop;
 
 /**
  *
@@ -16,11 +26,44 @@ import javax.swing.JTextPane;
 public class JTextPaneForVerboseInfo extends JScrollPane{
 
     private JTextPane text = new JTextPane();
+    private StyledDocument doc;
+    
+    private SimpleAttributeSet attributes;
+    
     public JTextPaneForVerboseInfo(){
+	StyleContext context = new StyleContext();
+	doc = new DefaultStyledDocument(context);
+
+	Style style = context.getStyle(StyleContext.DEFAULT_STYLE);
+	StyleConstants.setAlignment(style, StyleConstants.ALIGN_LEFT);
+	StyleConstants.setFontSize(style, 4);
+	StyleConstants.setSpaceAbove(style, 2);
+	StyleConstants.setSpaceBelow(style, 2);
+
+	attributes = new SimpleAttributeSet();
+	//StyleConstants.setBold(attributes, true);
+	int positions[] = { TabStop.ALIGN_BAR, TabStop.ALIGN_CENTER, TabStop.ALIGN_DECIMAL,
+			    TabStop.ALIGN_LEFT, TabStop.ALIGN_RIGHT };
+	TabStop tabstop = new TabStop(180, positions[3], TabStop.LEAD_DOTS);
+	TabSet tabset = new TabSet(new TabStop[] { tabstop });	
+	StyleConstants.setTabSet(attributes,tabset);
 	
+	text.setDocument(doc);
 	add(text);	
 	text.setEditable(false);	
-	setViewportView(text);
+	setViewportView(text);	
     }
     public JTextPane getTextEditor(){return text;}
+    
+    public void addEndTextnl(String str){
+	try {
+
+	    //Logger.getLogger("navigps").log(Level.SEVERE, null, str);
+	    doc.setParagraphAttributes(doc.getLength(), 1, attributes, false);
+	    doc.insertString(doc.getLength(), str+"\n", attributes);
+	} catch (BadLocationException ex) {
+	    Logger.getLogger("navigps").log(Level.SEVERE, null, ex);
+	}
+	
+    }
 }
