8d81eff38178aa06fa74def90eab9757c5e5c8e4
Grzegorz (vara) Warywoda <war29@wp.pl>
2008-10-17 20:40:00 +0200

    Add a class responsible for the visual part of the search services

--- src/app/gui/svgComponents/Canvas.java
+++ src/app/gui/svgComponents/Canvas.java
@@ -8,17 +8,28 @@ package app.gui.svgComponents;
 import app.utils.Utils;
 import config.MainConfiguration;
 import config.SVGConfiguration;
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import org.apache.batik.dom.svg.SVGOMPoint;
 import org.apache.batik.swing.JSVGCanvas;
-import org.apache.batik.swing.JSVGScrollPane;
 import org.w3c.dom.svg.SVGDocument;
 
 /**
@@ -61,6 +72,11 @@ public class Canvas extends JSVGCanvas{
     private SVGBridgeListeners listeners;
     private ZoomAndMove zoomListener;
         
+    /*
+     *	enebled/desibled sercher services
+     */
+    private boolean findSerices = true;
+    private Sercher sercher = new Sercher();
     
     public Canvas(SVGBridgeListeners listeners){
 		
@@ -77,8 +93,13 @@ public class Canvas extends JSVGCanvas{
         //addMouseListener(zoomListener);
         addMouseMotionListener(zoomListener);
 	this.listeners = listeners;
+	
+	setLayout(new GridLayout());
+	add(sercher);
+	addMouseMotionListener(sercher);
+	addMouseListener(sercher);
     }
-
+    
     @Override
     public void setURI(String uri){
 	listeners.setAbsoluteFilePath(uri);
@@ -88,6 +109,11 @@ public class Canvas extends JSVGCanvas{
     
     public JLabel getLabelViewMousePosyton() {
 	return labelViewMousePosyton;
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+	super.paintComponent(g);
     }
     
     public class ZoomAndMove extends MouseAdapter
@@ -110,10 +136,13 @@ public class Canvas extends JSVGCanvas{
 		    System.out.println("zoomOut");
 	    }
 	}
-	
+	@Override
+	public void mousePressed(MouseEvent e) {
+	    	
+	}
 	@Override
 	public void mouseDragged(MouseEvent e) {
-	    //throw new UnsupportedOperationException("Not supported yet.");
+	    
 	    
 	}
 
@@ -188,6 +217,87 @@ public class Canvas extends JSVGCanvas{
 	    removeMouseListener(zoomListener);
 	    //System.out.println("Usunieto listenera");		
 	}	
+    }
+    
+    private class Sercher extends JComponent implements MouseListener,MouseMotionListener{
+	
+	private Point.Double xy = new Point.Double(0,0);
+	private double radius=0;
+	private Point.Double currentPos = new Point.Double(0,0);
+	private boolean transformRepaint = true;
+	
+	public Sercher(){
+	    
+	}
+	@Override
+	public void paintComponent(Graphics g){
+	    super.paintComponent(g);
+	    Graphics2D g2 = (Graphics2D)g;
+	    AffineTransform mainTransform = g2.getTransform();
+	    if(transformRepaint){
+		System.out.println("auto repaint "+transformRepaint);
+		g2.setTransform(getRenderingTransform());		
+	    }
+	    
+	    float dash[] = { 10.0f };	    	    
+	    BasicStroke bs = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+	    Ellipse2D circle = new Ellipse2D.Double(-radius,-radius,radius*2,radius*2);
+	    Ellipse2D centerPiont = new Ellipse2D.Double(xy.x-2,xy.y-2,4,4);
+	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);		
+	    g2.setColor(Color.GREEN);		
+	    g2.fill(centerPiont);
+	    g2.setColor(Color.BLACK);		
+	    g2.drawLine((int)xy.x,(int)xy.y,(int)currentPos.x,(int)currentPos.y);		
+	    g2.setStroke(bs);
+	    g2.translate(xy.x,xy.y);		
+	    g2.draw(circle);				
+	    Color c = new Color(0,150,255,100);
+	    g2.setColor(c);
+	    g2.fill(circle);
+	    g2.translate(-xy.x,-xy.y);
+	    
+	    if(transformRepaint){
+		g2.setTransform(mainTransform);
+	    }
+	}
+	public void setCenterPoint(double x,double y){
+	    xy.setLocation(x, y);
+	}
+	public void setRadius(double r){
+	    radius =r;
+	}
+	public void setCurrentPosition(double x,double y){
+	    currentPos.setLocation(x, y);
+	    setRadius(xy.distance(currentPos));
+	}
+	public Point.Double getCenterPoint(){
+	    return xy;
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
+	public void mousePressed(MouseEvent e) {
+	    if(findSerices){
+		transformRepaint = false;
+		sercher.setCenterPoint(e.getX(), e.getY());
+		System.out.println(""+e.getX()+""+ e.getY());
+	    }
+	}
+	public void mouseReleased(MouseEvent e) {
+	}
+	public void mouseEntered(MouseEvent e) {	    
+	}
+	public void mouseExited(MouseEvent e) {	   
+	}
+	public void mouseDragged(MouseEvent e) {  
+	    if(findSerices){
+		sercher.setCurrentPosition(e.getX(), e.getY());		
+		repaint();
+		transformRepaint = true;
+	    }
+	}
+	public void mouseMoved(MouseEvent e) {	    
+	}
     }
     
 }
