e763292a1157f56ec77041e02b8aea932e7fb899
Grzegorz (vara) Warywoda <war29@wp.pl>
2008-12-22 12:12:57 +0100

    added some global var.
    Signed-off-by: Grzegorz (vara) Warywoda <war29@wp.pl>

--- src/config/GUIConfiguration.java
+++ src/config/GUIConfiguration.java
@@ -5,12 +5,42 @@
 
 package config;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 /**
  *
  * @author vara
  */
 public class GUIConfiguration {   
-    
+    private static Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
+
     public GUIConfiguration(){
+    }
+
+    /**
+     * @return the windowSize
+     */
+    public static Dimension getWindowSize() {
+        return windowSize;
+    }
+
+    /**
+     * @param windowSize the windowSize to set
+     */
+    public static void setWindowSize(Dimension winSize) {        
+        if(winSize.getWidth()!=0 && winSize.getHeight()!=0){
+            if(winSize.getWidth()<=getWindowSize().getWidth() &&
+               winSize.getHeight()<=getWindowSize().getHeight() ){
+
+                GUIConfiguration.windowSize = winSize;
+                return;
+            }
+            else System.out.println("The root window can not be greater than screen resolution !");
+
+        }else System.out.println("The root window size must be greater than zero !");
+
+        System.out.println("Default window size "+
+                (int)getWindowSize().getWidth()+","+(int)getWindowSize().getHeight());
     }
 }
--- src/config/MainConfiguration.java
+++ src/config/MainConfiguration.java
@@ -7,6 +7,7 @@ package config;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.PrintStream;
 
 /**
  *
@@ -14,6 +15,8 @@ import java.awt.Toolkit;
  */
 public class MainConfiguration {
     
+    public static final String NAVIGPS_VERSION = "0.5";
+
     private static boolean modeVerbose = false;
     private static boolean modeVerboseGui = false;
     public static final String OS_WINDOWS = "WINDOWS";
@@ -33,73 +36,76 @@ public class MainConfiguration {
 	return Toolkit.getDefaultToolkit().getScreenSize();
     }
     
-    public static String getOSName()
-    {
-	String osname = System.getProperty("os.name");
-	if (osname.toUpperCase().indexOf(MainConfiguration.OS_WINDOWS) != -1) {	
-	    return MainConfiguration.OS_WINDOWS;
-	}else if(osname.toUpperCase().indexOf(MainConfiguration.OS_LINUX) != -1){
-	    return MainConfiguration.OS_LINUX;
-	}else
-	    return MainConfiguration.OS_OTHER;
+    public static String getOSName(){
+        String osname = System.getProperty("os.name");
+        if (osname.toUpperCase().indexOf(MainConfiguration.OS_WINDOWS) != -1) {
+            return MainConfiguration.OS_WINDOWS;
+        }else if(osname.toUpperCase().indexOf(MainConfiguration.OS_LINUX) != -1){
+            return MainConfiguration.OS_LINUX;
+        }else
+            return MainConfiguration.OS_OTHER;
+        }
+    
+    public static void setMode(boolean verbose){	
+        modeVerbose = verbose;
     }
     
-    public static void setMode(boolean verbose)
-    {	
-	modeVerbose = verbose;
-    }
-    
-    public static synchronized boolean getMode()
-    {
-	return modeVerbose;
+    public static synchronized boolean getMode(){
+        return modeVerbose;
     }
     
     public static boolean isModeVerboseGui() {
-	return modeVerboseGui;
+        return modeVerboseGui;
     }
 
     public static void setModeVerboseGui(boolean aModeVerboseGui) {
-	modeVerboseGui = aModeVerboseGui;
+        modeVerboseGui = aModeVerboseGui;
     }
     
     public static void setPathChartToFile(String pathToFile) {
-	MainConfiguration.pathToFile = pathToFile;
+        MainConfiguration.pathToFile = pathToFile;
     }
 
     public static String getPathToConfigurationFile() {
-	return pathToConfigurationFile;
+        return pathToConfigurationFile;
     }
     public static String getPathToChartFile() {
-	return pathToFile;
+        return pathToFile;
     }
 
     public static void setPathToConfigurationFile(String pathToConfigurationFile) {
-	MainConfiguration.pathToConfigurationFile = pathToConfigurationFile;
+        MainConfiguration.pathToConfigurationFile = pathToConfigurationFile;
     }
 
     public static boolean isShowDocumentProperties() {
-	return showDocumentProperties;
+        return showDocumentProperties;
     }
 
     public static void setShowDocumentProperties(boolean showDocumentProperties) {
-	MainConfiguration.showDocumentProperties = showDocumentProperties;
+        MainConfiguration.showDocumentProperties = showDocumentProperties;
     }
     
     @Override
     public String toString(){
-	String ret="";
-	
-	if(MainConfiguration.getMode())
-	   ret = "Mode set loud ! aaaaaa";
-	else
-	    ret = "Set to silent mode";
-	ret+="\nSystem Operation "+getOSName();
-	if(pathToFile != null);
-	    ret+="\nset Path to svg document "+pathToFile;
-	if(pathToConfigurationFile!=null)
-	    ret+="\nset Path to configuration file "+pathToConfigurationFile;
-	ret+="\nShow Document Properties "+showDocumentProperties;
-    
-	return ret;
+        String ret="";
+
+        if(MainConfiguration.getMode())
+           ret = "Mode set loud ! aaaaaa";
+        else
+            ret = "Set to silent mode";
+        ret+="\nSystem Operation "+getOSName();
+        if(pathToFile != null);
+            ret+="\nset Path to svg document "+pathToFile;
+        if(pathToConfigurationFile!=null)
+            ret+="\nset Path to configuration file "+pathToConfigurationFile;
+        ret+="\nShow Document Properties "+showDocumentProperties;
+
+        return ret;
+    }
+
+    public static void printVersion(String str,PrintStream out){
+        if(out==null)
+            out = System.out;
+        out.println(str+MainConfiguration.NAVIGPS_VERSION);
     }
 }
