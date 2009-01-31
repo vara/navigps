55d52f533706ed327337dbb79cd2c7bbc5844fd5
Grzegorz (vara) Warywoda <war29@wp.pl>
2009-01-31 13:25:35 +0100

    added UI for button (testing)

---
+++ src/app/gui/buttons/ui/MyButtonUI.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package app.gui.buttons.ui;
+
+import javax.swing.plaf.ComponentUI;
+
+/**
+ * Created on 2009-01-06, 23:40:41
+ * @author vara
+ */
+public class MyButtonUI extends ComponentUI{
+
+}
