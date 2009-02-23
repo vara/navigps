2128560169720e9ae82c313224b3e9c0180446b6
Grzegorz (vara) Warywoda <war29@wp.pl>
2009-02-23 22:12:15 +0100

    new Version
    Signed-off-by: Grzegorz (vara) Warywoda <war29@wp.pl>

--- src/app/Version.java
+++ src/app/Version.java
@@ -8,7 +8,7 @@ public class Version
 {
     private static final String MAJOR_VALUE = "0";
     private static final String MINOR_VALUE = "5";
-    private static final String DOT_VALUE = "1";
+    private static final String DOT_VALUE = "2";
     private static final String versionNumber = MAJOR_VALUE + "." + MINOR_VALUE + "." + DOT_VALUE;
     private static final String versionName = "NaviGPS infant";
 
