d78e32a2170195963efdd5f665f77d6202d14abc
Grzegorz (vara) Warywoda <war29@wp.pl>
2009-04-06 04:52:30 +0200

    New version 0.5.5
    Signed-off-by: Grzegorz (vara) Warywoda <war29@wp.pl>

--- src/app/navigps/Version.java
+++ src/app/navigps/Version.java
@@ -2,13 +2,13 @@ package app.navigps;
 
 /**
  *
- * @author wara
+ * @author Grzegorz (wara) Warywoda
  */
 public class Version
 {
     private static final String MAJOR_VALUE = "0";
     private static final String MINOR_VALUE = "5";
-    private static final String DOT_VALUE = "4";
+    private static final String DOT_VALUE = "5";
     private static final String versionNumber = MAJOR_VALUE + "." + MINOR_VALUE + "." + DOT_VALUE;
     private static final String versionName = "NaviGPS infant";
 
