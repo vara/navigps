72a376e8e48db6d691df17c7ff289139fa2bf288
Grzegorz (vara) Warywoda <war29@wp.pl>
2009-04-08 16:15:14 +0200

    Added close timer to PoupMenu
    Signed-off-by: Grzegorz (vara) Warywoda <war29@wp.pl>

--- src/app/navigps/gui/MyPopupMenu.java
+++ src/app/navigps/gui/MyPopupMenu.java
@@ -1,43 +1,63 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package app.navigps.gui;
 
+import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Icon;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
+import javax.swing.Timer;
 
 /**
  *
- * @author vara
+ * @author Grzegorz (vara) Warywoda
  */
 public class MyPopupMenu extends JPopupMenu{
+
+    private Timer closeTimer;
+    private int closeDelay = 5000;
+
+    public MyPopupMenu(){
+        this(null);
+    }
     
-    /**
-     *
-     */
-    public MyPopupMenu()
-    {
+    public MyPopupMenu(String title){
+        super(title);
         setLightWeightPopupEnabled(true);
         setAlignmentX(100);
+
+        closeTimer = new Timer(closeDelay, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(isVisible())
+                    setVisible(false);
+            }
+        });
+
+        closeTimer.setRepeats(false);
     }
-    /**
-     *
-     * @param item
-     */
-    public void addToPopup(JMenuItem [] item)
-    {
+
+    public void setTimeToClose(int closeInit){
+        closeDelay = closeInit;
+        closeTimer.setDelay(closeDelay);
+    }
+
+    public void addToPopup(JMenuItem [] item){
         if(item.length>0){
             for(int i=0;i<item.length;i++)
                 add(item[i]);
         }
     }
-    
+
+    @Override
+    public void show(Component invoker, int x, int y) {
+        super.show(invoker, x, y);
+        closeTimer.start();
+    }
+
+
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -51,23 +71,11 @@ public class MyPopupMenu extends JPopupMenu{
         //g2.setPaint(gradient1);
         //g2.fill(rec1);
     }
-    
-    /**
-     *
-     * @param str
-     * @param icon
-     * @param mnemo
-     * @param skrot
-     * @param al
-     * @return
-     */
-    public static JMenuItem menuItem(String str, Icon icon, int mnemo, String skrot, ActionListener al)
-    {
+    public static JMenuItem menuItem(String str, Icon icon, int mnemo, String skrot, ActionListener al){
         JMenuItem mi = new JMenuItem(str, icon);
         mi.setMnemonic(mnemo);
         mi.setAccelerator(KeyStroke.getKeyStroke(skrot));
         mi.addActionListener(al);
         return mi;
-    }
-   
+    }   
 }
